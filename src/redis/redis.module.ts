5608068a27d15c25e80f2eee65c16498cba68ebb
iridescent <2389504513@qq.com>
2024-01-17 19:51:52 +0800

    ✨ feat: 新增Redis模块

---
+++ src/redis/redis.module.ts
@@ -0,0 +1,32 @@
+/*
+ * @Date: 2024-01-12 22:37:08
+ * @Description: Redis工具模块
+ */
+import { Global, Module } from '@nestjs/common';
+import { RedisService } from './redis.service';
+import { createClient } from 'redis';
+import { ConfigService } from '@nestjs/config';
+
+@Global()
+@Module({
+  providers: [
+    RedisService,
+    {
+      provide: 'REDIS_CLIENT',
+      inject: [ConfigService],
+      async useFactory(configService: ConfigService) {
+        const client = createClient({
+          socket: {
+            host: configService.get('redis_server_host'),
+            port: configService.get('redis_server_port'),
+          },
+          database: configService.get('redis_server_db'),
+        });
+        await client.connect();
+        return client;
+      },
+    },
+  ],
+  exports: [RedisService],
+})
+export class RedisModule {}
