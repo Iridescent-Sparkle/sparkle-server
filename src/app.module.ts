22ae7f15eea38f296ec297c228db7a919c17e5fc
iridescent <2389504513@qq.com>
2024-01-20 10:26:53 +0800

    ✨ feat: 集成mysql模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: [`./env.${process.env.NODE_ENV}`],
+      envFilePath: [`src/.env.${process.env.NODE_ENV}`],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'mysql',
+          host: configService.get('mysql_server_host'),
+          port: configService.get('mysql_server_port'),
+          username: configService.get('mysql_server_username'),
+          password: configService.get('mysql_server_password'),
+          database: configService.get('mysql_server_database'),
+          synchronize: true,
+          logging: true,
+          entities: [],
+          poolSize: 10,
+          connectorPackage: 'mysql2',
+          extra: { authPlugin: 'sha256_password' },
+        };
+      },
     }),
   ],
   controllers: [AppController],
