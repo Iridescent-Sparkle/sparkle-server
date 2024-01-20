28d930f3820d0e3956fff8ec9fa3a58a9ccc323f
iridescent <2389504513@qq.com>
2024-01-20 17:41:16 +0800

    ✨ feat: 集成Etcd作为分布式服务配置中心

--- libs/etcd/src/etcd.module.ts
+++ libs/etcd/src/etcd.module.ts
@@ -1,8 +1,59 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
+import { Etcd3, IOptions } from 'etcd3';
 import { EtcdService } from './etcd.service';
 
-@Module({
-  providers: [EtcdService],
-  exports: [EtcdService],
-})
-export class EtcdModule {}
+export interface EtcdModuleAsyncOptions {
+  useFactory?: (...args: any[]) => Promise<IOptions> | IOptions;
+  inject?: any[];
+}
+
+export const ETCD_CLIENT_TOKEN = 'ETCD_CLIENT';
+
+export const ETCD_CLIENT_OPTIONS_TOKEN = 'ETCD_CLIENT_OPTIONS';
+
+@Module({})
+export class EtcdModule {
+  public static forRoot(options?: IOptions): DynamicModule {
+    return {
+      module: EtcdModule,
+      providers: [
+        EtcdService,
+        {
+          provide: ETCD_CLIENT_OPTIONS_TOKEN,
+          useValue: options,
+        },
+        {
+          provide: ETCD_CLIENT_TOKEN,
+          useFactory(options: IOptions) {
+            const client = new Etcd3(options);
+            return client;
+          },
+          inject: [ETCD_CLIENT_OPTIONS_TOKEN],
+        },
+      ],
+      exports: [EtcdService],
+    };
+  }
+  public static forRootAsync(options: EtcdModuleAsyncOptions): DynamicModule {
+    return {
+      module: EtcdModule,
+      providers: [
+        EtcdService,
+        {
+          provide: ETCD_CLIENT_OPTIONS_TOKEN,
+          useFactory: options.useFactory,
+          inject: options.inject || [],
+        },
+        {
+          provide: ETCD_CLIENT_TOKEN,
+          useFactory(options: IOptions) {
+            const client = new Etcd3(options);
+            return client;
+          },
+          inject: [ETCD_CLIENT_OPTIONS_TOKEN],
+        },
+      ],
+      exports: [EtcdService],
+    };
+  }
+}
