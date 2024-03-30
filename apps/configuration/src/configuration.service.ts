9d2133db63d8a7bc0e3107ae5ee1efa1bcc3887a
iridescent <2389504513@qq.com>
2024-03-31 01:32:50 +0800

    🐎 ci: admin服务添加cicd

--- apps/configuration/src/configuration.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class ConfigurationService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
--- apps/configuration/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { ConfigurationModule } from './configuration.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(ConfigurationModule);
-  await app.listen(3000);
-}
-bootstrap();
