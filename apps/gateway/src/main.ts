1ada2e3f8e27e87451baaf2d4d4f9c684d558ed6
iridescent <2389504513@qq.com>
2024-04-05 18:44:30 +0800

    ✨ feat: 关闭安全策略

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -2,9 +2,10 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import rateLimit from 'express-rate-limit';
+// import rateLimit from 'express-rate-limit';
 import { HttpExceptionFilter } from 'filters/http-exception.filter';
-import helmet from 'helmet';
+// import helmet from 'helmet';
+// import csurf from 'csurf';
 import { FormatResponseInterceptor } from 'interceptors/format-response.interceptor';
 import { InvokeRecordInterceptor } from 'interceptors/invoke-record.interceptor';
 import { EtcdService } from '../../../libs/etcd/src/etcd.service';
@@ -14,11 +15,11 @@ import { RpcExceptionFilter } from 'filters/rpc-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(GatewayModule);
 
-  app.use(helmet());
+  // app.use(helmet());
   // app.use(csurf());
-  app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 2000 }));
-  app.set('trust proxy', ['loopback', 'linklocal', 'uniquelocal']);
-  app.enableCors({ origin: [/\.iridescent.icu$/] });
+  // app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 2000 }));
+  // app.set('trust proxy', ['loopback', 'linklocal', 'uniquelocal']);
+  // app.enableCors({ origin: [/\.iridescent.icu$/] });
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new FormatResponseInterceptor());
